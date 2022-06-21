3a0fdd35692f5e997ade7c75df730a6a7bb7d896
Yijia Dai <daiyijia@Yijias-MacBook-Pro.local>
2022-06-21 12:44:01 -0400

    Second Version without crypto.

--- make_env.py
+++ make_env.py
@@ -10,23 +10,13 @@ from multi_agent_env import MultiAgentEnv_algopricing
 
 def make_env_agents(agentnames, params=default_params, first_file=None, second_file=None, third_file=None):
     import agents
-    agentslist = []
 
-    for en, name in enumerate(agentnames):
-        ag = agents.load(name + ".py").Agent(en, params)
-        agentslist.append(ag)
-        try:
-            ag = agents.load(name + ".py").Agent(en, params)
-            agentslist.append(ag)
-        except Exception as e:
-            print('competitioncodeprefixprinting: ',
-                  'error creating agent: ', name, e)
-    assert len(
-        agentslist) == 2, "competitioncodeprefixprinting: Skipping game, at least one agent errored out"
-
+    agents = [
+        agents.load(name + ".py").Agent(en, params)
+        for en, name in enumerate(agentnames)
+    ]
     env = MultiAgentEnv_algopricing(
         params, agentnames, len(
             agentnames), first_file, second_file, third_file
     )
-    # customer_covariates_file=None, customer_noisyembeddings_file=None, customer_valuations_file=None
-    return env, agentslist
+    return env, agents
