09553f9d77acd93bfdedeb846f9014d5d547b85e
Yijia Dai <daiyijia@dhcp-vl2041-31718.eduroam.cornell.edu>
2022-06-29 11:59:51 -0400

    Add some pieces for running.

--- rl_price_competition_multi_agent_env_base.py
+++ rl_price_competition_multi_agent_env_base.py
@@ -23,15 +23,25 @@ sys.path.append(currentdir)
 class Seller(Agent):
     def __init__(
         self,
-        world, # don't know what's this yet
         seller_id, # needed when identifying which seller, should be 0,1,2...
         init=False, # identifier whether this seller has initiated
         n_sellers=2,
+        n_items=2
     ):
-        self.world = world
         self.n_sellers = n_sellers
+        self.n_items = n_items
         self.seller_id = seller_id
         self.init = init
+
+    @property
+    def observation_space(self):
+        return spaces.Box(low=float('-inf'), high=float('inf'), shape=(13,),
+                                    dtype=np.float32)
+
+    @property
+    def action_space(self):
+        return spaces.Box(low=0.0, high=float('inf'), shape=(self.n_sellers,self.n_items),
+                                    dtype=np.float32)
     
     def apply_action(self, one_action):
         action = [0 for _ in range(self.n_sellers)]
@@ -60,7 +70,11 @@ class MultiPriceCompetitionEnv:
         self.customer_embedding = 0
         sale = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
 
-        self.sellers = list(range(self.n_sellers))
+        self.seller_list = list(range(self.n_sellers))
+        self.sellers = [
+            Seller(id, n_sellers, n_items)
+            for id in self.seller_list
+        ]
         self.last_rewards = [0 for _ in range(self.n_sellers)]
         self.seller_profits = [0 for _ in range(self.n_sellers)]
         self.seller_profits_timeseries = [[] for _ in range(self.n_sellers)]
@@ -71,14 +85,8 @@ class MultiPriceCompetitionEnv:
 
         self.state = [self.customer_covariates, self.customer_embedding, sale, self.seller_profits]
 
-        # The action space is a box with two seller's prices for two items.
-        self.action_space = spaces.Box(low=0.0, high=float('inf'), shape=(self.n_sellers,self.n_items),
-                                    dtype=np.float32)
-
-        # The observation space is a box with the current demographic covariants for the customer.
-        self.observation_space = spaces.Box(low=float('-inf'), high=float('inf'), shape=(13,),
-                                    dtype=np.float32)
-
+        self.observation_space = [agent.observation_space for agent in self.sellers]
+        self.action_space = [agent.action_space for agent in self.sellers]
 
     def reset(self):
         """Reinitializes variables."""
@@ -106,8 +114,8 @@ class MultiPriceCompetitionEnv:
         """
          # action is array of size 4
         action = action.reshape(2)
-        assert self.sellers[agent_id].init is not False, agent_id
-        self.sellers[agent_id].apply_action(action)
+        assert self.seller_list[agent_id].init is not False, agent_id
+        self.seller_list[agent_id].apply_action(action)
         if is_last:
 
             eps = 1e-7  # for random tie-breaking
@@ -121,7 +129,7 @@ class MultiPriceCompetitionEnv:
             # for each item, loop through agents. get lowest price for agent that has capacity left, keep track of which item if any the customer is buying
             for item in range(self.n_items):
                 value = valuations[item]
-                for agent in range(self.n_agents):
+                for agent in range(self.seller_list):
                     util = value - action[agent][item]
                     if util >= 0 and util + (random.random() - 0.5) * eps > max_utility:
                         max_utility = util
@@ -129,10 +137,9 @@ class MultiPriceCompetitionEnv:
                         max_utility_agent = agent
             
             if max_utility_agent >= 0:
-                self.agent_profits[max_utility_agent] += action[
+                self.seller_profits[max_utility_agent] += action[
                     max_utility_agent
                 ][max_utility_item]
-                self.cumulative_buyer_utility += max_utility
                 sale = (
                     max_utility_item,
                     max_utility_agent,
@@ -140,13 +147,13 @@ class MultiPriceCompetitionEnv:
                 )
             else:
                 sale = (np.nan, np.nan, action)
-            for agent in range(self.n_agents):
-                self.agent_profits_timeseries[agent].append(
-                    self.agent_profits[agent])
+            for agent in range(self.n_sellers):
+                self.seller_profits_timeseries[agent].append(
+                    self.seller_profits[agent])
 
             # set rewards
             rewards = {}
-            for agent in self.agents:
+            for agent in self.seller_list:
                 if max_utility_agent == agent:
                     rewards[agent] = action[agent][max_utility_item]
                 else:
@@ -161,19 +168,17 @@ class MultiPriceCompetitionEnv:
             # flatten and define state
             self.state = [self.customer_covariates, self.customer_embedding, sale.flat, self.agent_profits]
 
-            assert self.observation_space.contains(self.state)
-
             return self.state, rewards, done
 
     def render(self, close_before=False, mode="human", close=False, time_update=10):
         if self.time % time_update == 0:
             if close_before:
                 plt.close()
-            for agent in range(self.n_agents):
-                name = "Agent {}: {}".format(agent, self.agent_names[agent])
+            for agent in range(self.n_sellers):
+                name = "Agent {}: {}".format(agent, self.seller_list[agent])
                 plt.plot(
                     list(range(self.time)),
-                    self.agent_profits_timeseries[agent],
+                    self.seller_profits_timeseries[agent],
                     label=name,
                 )
             plt.legend(frameon=False)
@@ -192,4 +197,4 @@ class MultiPriceCompetitionEnv:
         )
 
     def get_last_dones(self):
-        return dict(zip(self.sellers, self.last_dones))
+        return dict(zip(self.seller_list, self.last_dones))
